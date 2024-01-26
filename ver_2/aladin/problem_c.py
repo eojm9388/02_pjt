36760a270f7fa9738951a934cb8080a7e24eddd2
부울경_1반_어지민 <eojm9388@gmail.com>
2024-01-26 16:48:49 +0900

    ver_2 aladin problem_c

--- ver_2/aladin/problem_c.py
+++ ver_2/aladin/problem_c.py
@@ -3,8 +3,34 @@ from pprint import pprint
 
 
 def bestseller_book():
-    # 여기에 코드를 작성합니다.
-    pass
+    URL = 'http://www.aladin.co.kr/ttb/api/ItemSearch.aspx'
+    book_list = []
+    book_sale_title = []
+    params = {
+        'ttbkey': '알라딘api',
+        'Query': '파울로 코엘료',
+        'QueryType': 'Author',
+        'MaxResults': 20,
+        'start': 1,
+        'SearchTarget': 'Book',
+        'output': 'js',
+        'Version': '20131101',
+    }
+
+    response = requests.get(URL, params=params).json()
+
+    for book in response.get('item'):
+        book_sale = []
+
+        book_sale.append(book.get('salesPoint'))
+        book_sale.append(book.get('title'))
+        book_list.append(book_sale)
+
+    book_list.sort(reverse=True)
+
+    for book in book_list[:5]:
+        book_sale_title.append(book[1])
+    return book_sale_title
 
 
 # 아래의 코드는 수정하지 않습니다.
