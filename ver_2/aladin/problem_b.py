acd847a7821d27a628d18b4193a9902ae064ebe5
부울경_1반_어지민 <eojm9388@gmail.com>
2024-01-26 16:38:26 +0900

    ver_2 aladin problem_b

--- ver_2/aladin/problem_b.py
+++ ver_2/aladin/problem_b.py
@@ -3,9 +3,24 @@ from pprint import pprint
 
 
 def best_review_books():
-    # 여기에 코드를 작성합니다.
-    pass
+    URL = 'http://www.aladin.co.kr/ttb/api/ItemSearch.aspx'
+    book_list = []
+    params = {
+        'ttbkey': '알라딘api',
+        'Query': '파울로 코엘료',
+        'QueryType': 'Author',
+        'MaxResults': 20,
+        'start': 1,
+        'SearchTarget': 'Book',
+        'output': 'js',
+        'Version': '20131101',
+    }
 
+    response = requests.get(URL, params=params).json()
+    for book in response.get('item'):
+        if book.get('customerReviewRank') >= 9:
+            book_list.append(book)
+    return book_list
 
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
