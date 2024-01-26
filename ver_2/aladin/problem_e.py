cf19adc305c3582cd82eb4debbb426fabfda6787
부울경_1반_어지민 <eojm9388@gmail.com>
2024-01-26 17:55:19 +0900

    ver_2 aladin problem_e (진행중)

--- ver_2/aladin/problem_e.py
+++ ver_2/aladin/problem_e.py
@@ -3,8 +3,31 @@ from pprint import pprint
 
 
 def ebook_list(title):
-    # 여기에 코드를 작성합니다.
-    pass
+    URL = 'http://www.aladin.co.kr/ttb/api/ItemSearch.aspx'
+    book_list = []
+    params = {
+        'ttbkey': '알라딘api',
+        'Query': title,
+        'QueryType': 'title',
+        'MaxResults': 1,
+        'start': 1,
+        'SearchTarget': 'Book',
+        'output': 'js',
+        'Version': '20131101',
+    }
+
+    response = requests.get(URL, params=params).json()
+    if response.get('item') == []:
+        return None
+
+
+    params['MaxResults'] = 1
+    params['SearchTarget'] = 'eBook'
+    
+    response_ebook = requests.get(URL, params=params).json()
+
+    
+    return response_ebook.get('item')[0].get('priceSales')
 
 
 # 아래의 코드는 수정하지 않습니다.
